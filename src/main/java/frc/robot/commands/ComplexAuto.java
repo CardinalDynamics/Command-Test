d3a817f1e6fc2454458ac8a6a04971dbbcad2083
Aeris_Fox <aerothefox05@gmail.com>
2022-04-07 14:03:59 -0700

    documentation i guess

--- src/main/java/frc/robot/commands/ComplexAuto.java
+++ src/main/java/frc/robot/commands/ComplexAuto.java
@@ -15,16 +15,28 @@ import edu.wpi.first.wpilibj2.command.StartEndCommand;
 // I'll be honest, I have no clue what any of this does, but it works.
 
 public class ComplexAuto extends SequentialCommandGroup {
+    /**
+     * ComplexAuto is a command group that drives backwards for 2.5 seconds, and then shoots a ball.
+     * 
+     * @param driveSubsystem The drive subsystem
+     * @param elevatorSubsystem The elevator subsystem
+     * @param intakeSubsystem The intake subsystem
+     * @param climberSubsystem The climber subsystem
+     * @param shooterSubsystem The shooter subsystem
+     *
+     */
     public ComplexAuto(DriveSubsystem driveSubsystem, ElevatorSubsystem elevatorSubsystem, IntakeSubsystem intakeSubsystem, ClimberSubsystem climberSubsystem, ShooterSubsystem shooterSubsystem) {
         addCommands(
             
             // Thank you to Zack from 1540 for actually knowing how to write this.
+            // a command that starts and then ends i guess
             new StartEndCommand(
                 () -> driveSubsystem.drive(0.25, 0.25),
                 () -> driveSubsystem.drive(0, 0),
                 driveSubsystem
             ).withTimeout(2.5),
 
+            // nyoom
             race(
                 new WaitCommand(2).andThen(elevatorSubsystem.commandUp().withTimeout(3.5)),
                 shooterSubsystem.runShooter()
